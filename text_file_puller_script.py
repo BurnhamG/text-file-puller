d76d293e54fc0c7086777fd7d0161cf33a997288
BurnhamG <18239224+BurnhamG@users.noreply.github.com>
2017-09-24 10:59:07 -0500

    Moving toward proper processing of files based on rep

--- text_file_puller_script.py
+++ text_file_puller_script.py
@@ -1,11 +1,38 @@
 #! python3
 # This will pull text files from the database system
+from datetime.datetime import now
 import glob
 import openpyxl
 import os
 os.chdir('S:\CSR\Contract Renewal Text Files')
 sourceBook = openpyxl.load_workbook(glob.glob('*.xlsx'))
 sourceSheet = sourceBook.sheetnames[0]
+emptyCount = 0
+listOfFiles = glob.glob('*.txt')
+
+with open(os.path.join('DataFiles', 'CcList.txt'),
+          encoding='utf-8') as file:
+    listOfCCRecipients = [line.rstrip() for line in file]
+
+with open(os.path.join('DataFiles', 'NonConList.txt'),
+          encoding='utf-8') as file:
+    listOfNonConReps = [line.rstrip() for line in file]
+
+with open(os.path.join('DataFiles', 'RepsThatNeedCc.txt'),
+          encoding='utf-8') as file:
+    listOfRepsNeedingCc = [line.rstrip() for line in file]
+
+with open(os.path.join('DataFiles', 'NoProcess.txt'),
+          encoding='utf-8') as file:
+    listToAvoid = [line.rstrip() for line in file]
+
+for i in range(2, sourceSheet.max_row):  # Skips header row
+    # Check what column should be examined
+    if sourceSheet.cell(row=i, column=5) in listOfFiles:
+        sourceSheet.cell(row=i, column=9).value = \
+            now().strftime('%m-%d-%Y %H:%M:%S')
+    elif sourceSheet.cell(row=i, column=8) in listToAvoid:
+        sourceSheet.cell(row=i, column=9).value = 'IGNORED'
 
 # See if there is a way to track what text files have already been pulled.
 # I may have to have the macro call this program after the source sheet is
@@ -18,4 +45,4 @@ sourceSheet = sourceBook.sheetnames[0]
 # to pull all files. Could also nest these options.
 
 # Go through, pulling text files and saving them under the contract name.
-# Make sure this also pulls the non-contract items if that applies
+# Make sure this also pulls the non-contract items if that applies.
