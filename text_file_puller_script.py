755c4e3715e388ad0ea3312655dc957e1408ad30
BurnhamG <18239224+BurnhamG@users.noreply.github.com>
2017-09-24 13:51:15 -0500

    Continue to develop how the program will interface with the database.

--- text_file_puller_script.py
+++ text_file_puller_script.py
@@ -4,6 +4,8 @@ from datetime.datetime import now
 import glob
 import openpyxl
 import os
+import pyautogui
+from pywinauto import application
 import time
 
 os.chdir('S:\CSR\Contract Renewal Text Files')
@@ -12,6 +14,8 @@ sourceSheet = sourceBook.sheetnames[0]
 emptyCount = 0
 listOfFiles = glob.glob('*.txt')
 listOfFiles_dict = {}
+
+print('Gathering a list of files that already exist...')
 
 for i in listOfFiles:
     listOfFiles_dict[i] = time.localtime(os.stat(i).st_mtime)
@@ -32,37 +36,44 @@ with open(os.path.join('DataFiles', 'NoProcess.txt'),
           encoding='utf-8') as file:
     listToAvoid = [line.rstrip() for line in file]
 
+# Checks for files that have already been pulled
 for i in range(2, sourceSheet.max_row):  # Skips header row
+
     # Check what column should be examined
-    if sourceSheet.cell(row=i, column=5) in listOfFiles:
+    if sourceSheet.cell(row=i, column=5) + '.txt' in listOfFiles:
         modified_date = \
             ' '.join('-'.join(listOfFiles_dict[
                               sourceSheet.cell(row=i, column=5).value].tm_mon,
                               listOfFiles_dict[
                               sourceSheet.cell(row=i, column=5).value].tm_mday,
                               listOfFiles_dict[
-                              sourceSheet.cell(row=i, column=5).value].tm_year)
-                     , ':'.join(listOfFiles_dict[
-                                sourceSheet.cell(row=i, column=5).value].tm_hour,
-                                listOfFiles_dict[
-                                sourceSheet.cell(row=i, column=5).value].tm_min,
-                                listOfFiles_dict[
-                                sourceSheet.cell(row=i, column=5).value].tm_sec))
-
+                              sourceSheet.cell(row=i, column=5).value].tm_year),
+                     ':'.join(listOfFiles_dict[
+                         sourceSheet.cell(row=i, column=5).value].tm_hour,
+                         listOfFiles_dict[
+                         sourceSheet.cell(row=i, column=5).value].tm_min,
+                         listOfFiles_dict[
+                         sourceSheet.cell(row=i, column=5).value].tm_sec))
         sourceSheet.cell(row=i, column=9).value = modified_date
-    elif sourceSheet.cell(row=i, column=8) in listToAvoid:
+
+    # Checks for text files to ignore
+    if sourceSheet.cell(row=i, column=8) in listToAvoid:
         sourceSheet.cell(row=i, column=9).value = 'IGNORED'
 
+# Ask the user for the first contract they would like to pull, or give the user
+# the option of inputting a list of contracts they would like to pull
+# (i.e. ones that came out blank) or leaving the input empty to pull all files.
 
-# See if there is a way to track what text files have already been pulled.
-# I may have to have the macro call this program after the source sheet is
-# formatted and then add "pulled" to a cell through this program as I pull
-# text files.
+print('What is the first contract you would like to pull? Leave this empty to \
+        pull all text files.')
+startContract = input()
+
+if startContract.strip():
+    for i in range(2, sourceSheet.max_row):
+        if sourceSheet.cell(row=i, column=5) == startContract:
+            # Here is where I need to set focus on the window and then input
+
 # Setting current time = now().strftime('%m-%d-%Y %H:%M:%S')
-# Maybe also ask the user for the first contract they would like to pull,
-# or give the user the option of inputting a list of contracts they would
-# like to pull (i.e. ones that came out blank) or leaving the input empty
-# to pull all files. Could also nest these options.
 
 # Go through, pulling text files and saving them under the contract name.
 # Make sure this also pulls the non-contract items if that applies.
